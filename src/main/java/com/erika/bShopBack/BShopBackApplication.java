fc442aa53e351c5d2afad975166b0826d62a70d6
erika-hortencia <erika.cardoso@usp.br>
2025-04-06 15:40:31 -0300

    Project created

---
+++ src/main/java/com/erika/bShopBack/BShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.erika.bShopBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BShopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erika/bShopBack/BShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erika.bShopBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
